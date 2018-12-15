d97666dafb59e7ed1839f26e711ebc2c3dc696b1
rchaser53 <tayoshizawa29@gmail.com>
2018-12-16 00:52:15 +0900

    [other] add the memo for never type

---
+++ memo.rs
@@ -0,0 +1,7 @@
+// #![feature(never_type)]
+// https://stackoverflow.com/questions/51832396/why-does-rust-have-a-never-primitive-type
+// fn main() -> () {
+//   let _x: ! = {
+//       return ();
+//   };
+// }
